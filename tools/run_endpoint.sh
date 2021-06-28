49832f47cd263b506df34587d68869daf3bdb4dd
Mathis Engelbart <mathis.engelbart@gmail.com>
2021-06-28 12:41:46 +0200

    Rename endpoint parameters

--- tools/run_endpoint.sh
+++ tools/run_endpoint.sh
@@ -8,9 +8,9 @@ if [ "$ROLE" == "sender" ]; then
     # Wait for the simulator to start up.
     /wait-for-it.sh sim:57832 -s -t 10
     echo "Starting RTQ sender..."
-    echo "Client params: $CLIENT_PARAMS"
+    echo "Sender params: $SENDER_PARAMS"
     echo "Test case: $TESTCASE"
-    QUIC_GO_LOG_LEVEL=debug ./sender $CLIENT_PARAMS $REQUESTS
+    QUIC_GO_LOG_LEVEL=debug ./sender $SENDER_PARAMS $VIDEOS
 else
     echo "Running RTQ receiver."
     QUIC_GO_LOG_LEVEL=debug ./receiver "$@"
